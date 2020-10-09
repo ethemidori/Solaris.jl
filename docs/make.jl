e7dbdd315672d91efbf632c59bff4b49f9281853
Tianbai Xiao <tianbaixiao@gmail.com>
2020-10-09 12:19:42 +0200

    m

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,13 @@
 using Pkg
 Pkg.add("Documenter")
+push!(LOAD_PATH,"../src/")
 using Documenter, Solaris
 
 makedocs(
     sitename = "Solaris.jl",
 )
 
-deploydocs(
-    repo = "github.com/ethemidori/Solaris.jl.git",
-    target = "build",
-)
+#deploydocs(
+#    repo = "github.com/ethemidori/Solaris.jl.git",
+#    target = "build",
+#)
